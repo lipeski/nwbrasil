aa898042111eeacb1a33d09f9df57537d56d79ee
Felipe <lipe_getnerski@hotmail.com>
2020-10-29 00:19:02 -0300

    add functions for firebase and express for api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const app = require("express")();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const db = admin.firestore().collection("guilds")
+
+app.get("/guilds", (request, response) => {
+    db.get()
+    .then((docs)=>{
+        let guilds = [];
+
+        docs.forEach((element) => {
+            guilds.push({id: element.id, nome: element.data().name, membros: element.data().members, servidor: element.data().server})
+        });
+        return response.json(guilds);
+    }).catch(error => {
+        return console.error(error);
+    })
+});
+
+app.post("/guilds", (request, response) =>{
+    const newGuild = {
+        name: request.body.name,
+        members: request.body.members,
+        server: request.body.server
+    }
+    db.add(newGuild)
+    .then(()=> {
+        return response.status(200)
+    }).catch((error)=>{return error})
+});
+
+exports.api = functions.https.onRequest(app);
